eb3a18947c15fb94097f60bf68b5683df6fdf288
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-11-12 14:07:34 +0000

    Added cart functionality to ApplicationController and Product Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+
+  def add
+    @product = Product.find(params[:id])
+
+    cart << @product
+  end
 end
